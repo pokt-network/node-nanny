873514e0d372920767bd77ac6bd978c59c30f5a9
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-22 10:02:30 -0700

    ready to run in aws

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,36 @@ import { hexToDec } from "./utils";
 
 dotenv.config();
 
+enum ErrorConditions {
+  FORCED = "forced",
+}
+
+enum SyncStatusErrorLevel {
+  SYNCHRONIZED = "SYNCHRONIZED",
+  WARNING = "WARNING",
+  ERROR = "ERROR",
+}
+
+enum ConfigParams {
+  SYNC = "Sync",
+}
+
+enum SupportedChains {
+  ETH = "Eth",
+}
+
 class ProofOfConcept {
   private log: CloudWatchLogs;
   private external: ExternalHeight;
   private internal: InternalHeight;
-  private disovery: Discovery;
+  private discovery: Discovery;
   private config: ConfigManager;
   private today: string;
   constructor() {
     this.log = new CloudWatchLogs();
     this.external = new ExternalHeight();
     this.internal = new InternalHeight();
-    this.disovery = new Discovery();
+    this.discovery = new Discovery();
     this.config = new ConfigManager();
     this.today = new Date().toDateString();
   }
@@ -39,8 +57,26 @@ class ProofOfConcept {
     if (!streamExist) {
       await this.log.createLogStream({ logGroupName, logStreamName: this.today });
     }
+    //Optimization, we should not have to call the describe method on every invocation since the next token is supply by the putLogs call
+    return this.log.getSequenceTokenForLogStream({ logGroupName, logStreamName: this.today });
+  }
 
-    return this.log.getSequenceTokenforLogStream({ logGroupName, logStreamName: this.today });
+  //Optimization, eventually we will want to fetch the configuration via subscription or a on less frequent interval
+  async evaluateSyncHealth({
+    chain,
+    internal,
+    external,
+  }: {
+    chain: SupportedChains;
+    internal: number;
+    external: number;
+  }) {
+    const currentConfig = await this.config.getParam({ chain, param: ConfigParams.SYNC });
+    if (currentConfig === ErrorConditions.FORCED) {
+      return SyncStatusErrorLevel.ERROR;
+    }
+
+    return external - internal > Number(currentConfig) ? SyncStatusErrorLevel.ERROR : SyncStatusErrorLevel.SYNCHRONIZED;
   }
 
   async checkNode({ name, type, ip, port, https }) {
@@ -58,10 +94,17 @@ class ProofOfConcept {
     internal = hexToDec(internal);
     external = hexToDec(external);
 
+    const status = await this.evaluateSyncHealth({
+      chain: SupportedChains.ETH,
+      internal,
+      external,
+    });
+
     const message = JSON.stringify({
       internal,
       external,
       delta: external - internal,
+      status,
     });
 
     const sequenceToken = await this.setupLogs(name);
@@ -76,7 +119,7 @@ class ProofOfConcept {
   }
 
   async main() {
-    const nodes = await this.disovery.getListOfNodes();
+    const nodes = await this.discovery.getListOfNodes();
 
     setInterval(async () => {
       for (const { name, type, ip, port, https } of nodes) {
