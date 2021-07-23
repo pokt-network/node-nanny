9342723bfa0214cae3265fee7b04f9ec0e00a99d
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-23 16:50:36 -0700

    feat: disovery via csv or tags

--- src/services/discovery.ts
+++ src/services/discovery.ts
@@ -1,5 +1,6 @@
-import path from "path";
+import AWS from "aws-sdk";
 import csv from "csvtojson";
+import path from "path";
 
 //todo cast https to bool
 interface Nodes {
@@ -10,11 +11,72 @@ interface Nodes {
   https: string;
 }
 
-const sourcePath = path.resolve(__dirname, "../../nodes.csv");
+enum Source {
+  CSV = "csv",
+  TAG = "tag",
+}
+
+enum Prefix {
+  BLOCKCHAIN = "blockchain",
+  NAME = "Name",
+}
+const nodes = path.resolve(__dirname, "../../nodes.csv");
 
 class Service {
-  async getListOfNodes(): Promise<Nodes[]> {
-    return await csv().fromFile(sourcePath);
+  private source: Source;
+  private sourcePath: string;
+  private client: AWS.EC2;
+  constructor({ source = Source.TAG, sourcePath = nodes }) {
+    this.source = source;
+    this.sourcePath = sourcePath;
+  }
+
+  initEC2() {
+    this.client = new AWS.EC2({ region: "us-east-2" });
+  }
+
+  getInstanceName(tags) {
+    const [{ Value }] = tags.filter(({ Key }) => Key === Prefix.NAME);
+    return Value;
+  }
+  async getNodesListFromTags() {
+    const nodes = [];
+    this.initEC2();
+    const { Reservations } = await this.client
+      .describeInstances({
+        Filters: [
+          {
+            Name: "tag:Type",
+            Values: ["node-host"],
+          },
+        ],
+      })
+      .promise();
+
+    for (const { Instances } of Reservations) {
+      const [{ PrivateIpAddress: ip, Tags }] = Instances;
+
+      const instanceName = this.getInstanceName(Tags);
+
+      for (const { Key, Value } of Tags) {
+        if (Key.includes(Prefix.BLOCKCHAIN)) {
+          const [, type] = Key.split("-");
+          const name = `${instanceName}/${type.toLowerCase()}`;
+          const port = Value;
+          nodes.push({ name, type, port, https: "false" });
+        }
+      }
+    }
+    return nodes;
+  }
+
+  async getListOfNodes(): Promise<any> {
+    if (this.source === Source.TAG) {
+      return await this.getNodesListFromTags();
+    }
+    if (this.source === Source.CSV) {
+      return await csv().fromFile(this.sourcePath);
+    }
   }
 }
 
