c763ab0a30e64f3a46bb9a16412a7da87998f885
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-23 09:57:23 -0700

    wip

--- src/services/internalheight.ts
+++ src/services/internalheight.ts
@@ -14,7 +14,7 @@ class Service {
 
   async getBlockHeight({ ip, port, chain = "eth", https = "false" }) {
     let url: string;
-    https === "true" ? (url = `https://${ip}`) : (url = `http://${ip}:${port}`);
+        https === "true" ? (url = `https://${ip}`) : (url = `http://${ip}:${port}`);
     try {
       const { data } = await this.rpc.post(url, {
         jsonrpc: "2.0",
