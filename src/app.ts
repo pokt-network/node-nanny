25db0f819c093be224bdda40dbcb83cd3a3df9e3
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 15:41:15 -0700

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import { Discover } from "./services";
 import { DiscoverTypes } from "./types";
 import { Health } from "./services";
+import { HealthTypes } from "./types";
 
 export class App {
   private discover: Discover;
   private health: Health;
+  supported: string[];
   constructor() {
     this.discover = new Discover({ source: DiscoverTypes.Source.TAG });
     this.health = new Health();
+    this.supported = Object.keys(HealthTypes.EthVariants)
   }
 
   async main() {
-    const nodes = await this.discover.getListOfNodes();
+    let nodes = await this.discover.getListOfNodes();
+    nodes = nodes.filter(({type})=> this.supported.includes(type))
     console.table(nodes)
     for (const { name, type, ip, port } of nodes) {
       const health = await this.health.getNodeHealth({ name, type, ip, port });
