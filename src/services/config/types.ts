08bc252472bf14fa2895be9b690d8890d7659e94
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 09:39:52 -0700

    add handling for multiple external nodes for blockheight, jest, restructured services

---
+++ src/services/config/types.ts
@@ -0,0 +1,6 @@
+export enum ConfigPrefix {
+    COMMON_URL = "/pocket/monitoring/config/common/url",
+    CHAIN_HEALTH = "/pocket/monitoring/config/health",
+    EXTERNAL_ENDPOINTS_ETH = "/pocket/monitoring/config/externalEndpoints/eth",
+  }
+  
---
+++ src/services/health/types.ts
@@ -0,0 +1,3 @@
+export enum NCResponse {
+    SUCCESS = "succeeded!",
+  }
