a2fcf94eef01c88608eb3ae085841884b245f7d4
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 11:06:44 -0700

    add dd process monitoring

--- src/services/health/types.ts
+++ src/services/health/types.ts
@@ -1,3 +1,18 @@
 export enum NCResponse {
-    SUCCESS = "succeeded!",
-  }
+  SUCCESS = "succeeded!",
+}
+
+export enum EthVariants {
+  BSC = "BSC",
+  POL = "POL",
+  RIN = "RIN",
+  ROP = "ROP",
+  GOE = "GOE",
+  KOV = "KOV",
+  XDAI = "XDAI",
+  FUS = "FUS",
+}
+
+export enum NonEthVariants {
+  AVA = "AVA",
+}
