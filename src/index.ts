64df5c3cde081d1e18fe7447c2c16b957bf4af6e
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 11:06:44 -0700

    add dd process monitoring

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { App } from "./app";
+import StatsD from "hot-shots";
+import "./tracer";
+
+enum Config {
+  INTERVAL = 10000,
+}
+
+const app = new App();
+const statsd = new StatsD({
+  globalTags: { env: process.env.NODE_ENV },
+});
+
+setInterval(async () => {
+  await app.main().then(console.log);
+  statsd.increment("process.interval");
+}, Config.INTERVAL);
