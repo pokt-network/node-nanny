e9d15f5c83cc2084c107452f454fd1f7da5f7c8c
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 15:10:21 -0700

    wip

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,4 @@ export { Service as Alerts } from "./alerts";
 export { Service as Config } from "./config";
 export { Service as Discover } from "./discover";
 export { Service as Health } from "./health";
-export { Service as Logs } from "./logs";
+export { Service as Logs } from "./log";
