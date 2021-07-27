2b570003c3eac472be5efbf2194a6d0e2a5959a5
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 15:10:21 -0700

    wip

---
+++ src/services/log/service.ts
@@ -0,0 +1,106 @@
+import AWS from "aws-sdk";
+import { GroupStreamParams, PutLogParams, LogGroupPrefix } from "./types";
+
+export class Service {
+  private client: AWS.CloudWatchLogs;
+  private today: string;
+  constructor() {
+    this.today = new Date().toDateString();
+    this.client = new AWS.CloudWatchLogs({
+      region: "us-east-2",
+    });
+  }
+
+  async doesLogGroupExist(name: string): Promise<boolean> {
+    try {
+      const { logGroups } = await this.client
+        .describeLogGroups({ logGroupNamePrefix: name })
+        .promise();
+      const [logGroup] = logGroups;
+      return logGroup.logGroupName === name;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async createLogGroup(logGroupName: string): Promise<object> {
+    try {
+      return await this.client.createLogGroup({ logGroupName }).promise();
+    } catch (error) {
+      throw new Error(`could not create log group ${error}`);
+    }
+  }
+
+  async doesLogStreamExist({ logGroupName, logStreamName }: GroupStreamParams): Promise<boolean> {
+    try {
+      const { logStreams } = await this.client
+        .describeLogStreams({
+          logGroupName,
+          logStreamNamePrefix: logStreamName,
+        })
+        .promise();
+      const [logStream] = logStreams;
+      return logStream.logStreamName === logStreamName;
+    } catch (error) {
+      return false;
+    }
+  }
+  async createLogStream({ logGroupName, logStreamName }: GroupStreamParams): Promise<object> {
+    try {
+      return await this.client.createLogStream({ logGroupName, logStreamName }).promise();
+    } catch (error) {
+      throw new Error(`could not create log stream ${error}`);
+    }
+  }
+
+  async writeToLogStream({
+    logGroupName,
+    logStreamName,
+    logEvents,
+    sequenceToken,
+  }: PutLogParams): Promise<AWS.CloudWatchLogs.PutLogEventsResponse> {
+    try {
+      return await this.client
+        .putLogEvents({ logGroupName, logStreamName, logEvents, sequenceToken })
+        .promise();
+    } catch (error) {
+      throw new Error(`could not write to log stream ${error}`);
+    }
+  }
+
+  async getSequenceTokenForLogStream({
+    logGroupName,
+    logStreamName: name,
+  }: GroupStreamParams): Promise<string> {
+    try {
+      const { logStreams } = await this.client
+        .describeLogStreams({ logGroupName, logStreamNamePrefix: name })
+        .promise();
+
+      const [{ logStreamName, uploadSequenceToken }] = logStreams;
+
+      if (logStreamName !== name) {
+        throw new Error(`could not find specific log stream but found similar stream name`);
+      }
+      return uploadSequenceToken;
+    } catch (error) {
+      throw new Error(`could not find log stream ${error}`);
+    }
+  }
+  async setupLogs(name) {
+    const logGroupName = `${LogGroupPrefix.BASE}${name}`;
+    const groupExists = await this.doesLogGroupExist(logGroupName);
+    if (!groupExists) {
+      await this.createLogGroup(logGroupName);
+    }
+    const streamExist = await this.doesLogStreamExist({
+      logGroupName,
+      logStreamName: this.today,
+    });
+    if (!streamExist) {
+      await this.createLogStream({ logGroupName, logStreamName: this.today });
+    }
+    //Optimization, we should not have to call the describe method on every invocation since the next token is supply by the putLogs call
+    return this.getSequenceTokenForLogStream({ logGroupName, logStreamName: this.today });
+  }
+}
