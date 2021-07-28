7ffac4a78ac0c4e9ba4d918327f34ca9067af7c7
Ubuntu <ubuntu@ip-10-0-0-102.us-east-2.compute.internal>
2021-07-28 23:52:49 +0000

    include peer and remote urls in discovery fetch and output, redo best blockheight algo to account for all cases

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,14 @@ export const wait = async (ms: number): Promise<void> => {
 export const hexToDec = (hexString) => {
   return parseInt(hexString, 16);
 };
+export const compare = (a, b, field) => {
+  const A = a[field].toUpperCase();
+  const B = b[field].toUpperCase();
+  let comparison = 0;
+  if (A > B) {
+    comparison = 1;
+  } else if (A < B) {
+    comparison = -1;
+  }
+  return comparison;
+}
