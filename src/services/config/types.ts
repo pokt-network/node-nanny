544ce9bdb87fc845eb7d801a916bd7f8c03ce9c1
JonathonJulian <jonathon.fritz@nextwave.guru>
2021-07-27 20:39:26 -0700

    wip

--- src/services/config/types.ts
+++ src/services/config/types.ts
@@ -6,4 +6,11 @@ export enum ConfigPrefix {
 export enum ExternalEndPoints {
   ETH = "/pocket/monitoring/config/externalEndpoints/eth",
   BSC = "/pocket/monitoring/config/externalEndpoints/bsc",
+  FUS = "/pocket/monitoring/config/externalEndpoints/fus",
+  GOE = "/pocket/monitoring/config/externalEndpoints/goe",  
+  KOV = "/pocket/monitoring/config/externalEndpoints/kov",
+  POL = "/pocket/monitoring/config/externalEndpoints/pol",
+  RIN = "/pocket/monitoring/config/externalEndpoints/rin",
+  ROP = "/pocket/monitoring/config/externalEndpoints/eth",
+  XDAI = "/pocket/monitoring/config/externalEndpoints/bsc",
 }
