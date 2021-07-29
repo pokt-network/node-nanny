e3d0f3a70badf3a224ec6c998f924e771ba02b0d
Jonathon Fritz <jonathon.fritz@nextwave.guru>
2021-07-29 16:02:27 -0700

    function mvp (#2)
    * inital project setup
    
    * feature: added cloudwatch service and methods required to check and create logstreams
    
    * WIP: added block height checks and more supporting services, basic cli for config, main monitoring function for POC
    
    * resolved next token issue
    
    * ready to run in aws
    
    * change node list to internal nodes
    
    * update case
    
    * move csv to root
    
    * add some logging
    
    * add some more logging
    
    * add some more logging
    
    * fix
    
    * wip
    
    * feat: disovery via csv or tags
    
    * fix:forgot port
    
    * wip: added nc, discord and pd to services, check if nodes online before scanning.
    
    * add handling for multiple external nodes for blockheight, jest, restructured services
    
    * file changes
    
    * add dd process monitoring
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * include peer and remote urls in discovery fetch and output, redo best blockheight algo to account for all cases
    
    * re-enable main method, add variance for supported chains
    
    * added alerting
    
    Co-authored-by: Ubuntu <ubuntu@ip-10-0-0-102.us-east-2.compute.internal>

---
+++ src/services/config/types.ts
@@ -0,0 +1,18 @@
+export enum ConfigPrefix {
+  COMMON_URL = "/pocket/monitoring/config/common/url",
+  CHAIN_HEALTH = "/pocket/monitoring/config/health",
+  EXTERNAL_ENDPOINT = "/pocket/monitoring/config/externalEndpoints",
+}
+
+export enum ExternalEndPoints {
+  ETH = "/pocket/monitoring/config/externalEndpoints/eth",
+  BSC = "/pocket/monitoring/config/externalEndpoints/bsc",
+  FUS = "/pocket/monitoring/config/externalEndpoints/fus",
+  GOE = "/pocket/monitoring/config/externalEndpoints/goe",
+  KOV = "/pocket/monitoring/config/externalEndpoints/kov",
+  POL = "/pocket/monitoring/config/externalEndpoints/pol",
+  RIN = "/pocket/monitoring/config/externalEndpoints/rin",
+  ROP = "/pocket/monitoring/config/externalEndpoints/eth",
+  XDAI = "/pocket/monitoring/config/externalEndpoints/bsc",
+  AVA = "/pocket/monitoring/config/externalEndpoints/ava",
+}
