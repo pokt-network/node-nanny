146e05d250ff01bc449fe0525b9c08a172a0dc65
Ubuntu <ubuntu@ip-10-0-0-102.us-east-2.compute.internal>
2021-07-29 22:54:49 +0000

    added alerting

--- src/services/alert/alerts.test.ts
+++ src/services/alert/alerts.test.ts
@@ -1,3 +1,9 @@
-test("", () => {
+import { Service } from "./service";
+import { AlertChannel, Titles } from "./types";
+
+const alert = new Service();
+
+test.only("", async () => {
+//  await alert.sendAlert({channel: AlertChannel.BOTH, title: Titles.OFFLINE, details:`this node is offfline!!`});
   expect(3).toBe(3);
 });
