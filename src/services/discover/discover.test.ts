6296cb029ad826212bf4db96b413017765630662
jonathon <john@pokt.network>
2021-08-03 20:04:20 +0000

    wip: report on pocket nodes per host

--- src/services/discover/discover.test.ts
+++ src/services/discover/discover.test.ts
@@ -4,8 +4,8 @@ import { Source } from "./types";
 const discover = new Service({ source: Source.TAG });
 
 test.skip("should fetch pocket nodes and peers", async () => {
-  const nodes = await discover.getNodes()
-  console.log(nodes);
+  const nodes = await discover.getPocketNodes()
+ // console.log(nodes);
 
   expect(3).toEqual(3);
 });
