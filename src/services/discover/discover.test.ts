ae3a7881506316b7d0a74b54d03cbc64d9956a14
jonathon <john@pokt.network>
2021-08-04 18:11:08 +0000

    integrate data and pocket monitoring, fix race condition

--- src/services/discover/discover.test.ts
+++ src/services/discover/discover.test.ts
@@ -4,7 +4,7 @@ import { Source } from "./types";
 const discover = new Service({ source: Source.TAG });
 
 test.skip("should fetch pocket nodes and peers", async () => {
-  const nodes = await discover.getPocketNodes()
+ // const nodes = await discover.getPocketNodes()
  // console.log(nodes);
 
   expect(3).toEqual(3);
