8e5a59d29a41190e1965acf4db4e72a0b2a70f9c
jonathon <john@pokt.network>
2021-08-06 16:45:02 +0000

    remove alerting from main app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-import { Alert, Discover, Health, Log } from "./services";
-import { DiscoverTypes, HealthTypes, AlertTypes } from "./types";
+import { Discover, Health, Log } from "./services";
+import { DiscoverTypes } from "./types";
 import { wait } from "./utils";
 
 export class App {
-  private alert: Alert;
   private discover: Discover;
   private health: Health;
   private log: Log;
   constructor() {
-    this.alert = new Alert();
     this.discover = new Discover({ source: DiscoverTypes.Source.TAG });
     this.log = new Log();
     this.health = new Health();
@@ -30,23 +28,6 @@ export class App {
         const { name } = health;
         console.info({ name, health });
         await this.log.write({ message, name });
-        if (health.status === HealthTypes.ErrorStatus.ERROR) {
-          if (health.conditions === HealthTypes.ErrorConditions.OFFLINE) {
-            await this.alert.sendAlert({
-              channel: AlertTypes.AlertChannel.DISCORD,
-              title: AlertTypes.Titles.OFFLINE,
-              details: `Node ${name} is currently offline`,
-            });
-          }
-
-          if (health.conditions === HealthTypes.ErrorConditions.NOT_SYNCHRONIZED) {
-            await this.alert.sendAlert({
-              channel: AlertTypes.AlertChannel.DISCORD,
-              title: AlertTypes.Titles.NOT_SYNCHRONIZED,
-              details: `Node ${name} is currently not in synch ${message}}`,
-            });
-          }
-        }
       }
     }
     return;
