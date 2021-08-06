2d55cee999200774e24d4edf6a5f41029aeb68b5
jonathon <john@pokt.network>
2021-08-06 16:16:52 +0000

    add datadog service methods to add and remove batch of monitors

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export { Service as Alert } from "./alert";
 export { Service as Config } from "./config";
+export { Service as DataDog } from "./datadog";
 export { Service as Discover } from "./discover";
 export { Service as Health } from "./health";
 export { Service as Log } from "./log";
