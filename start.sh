fe6c673692e4b1183d68203630a3fa689ab05f51
jonathon <john@pokt.network>
2021-08-09 21:22:54 +0000

    add full nvm path

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-cd /home/ubuntu/run/
-pm2 kill all
-yarn
-yarn build
-pm2 start dist/index.js
-pm2 start dist/api
+cd /home/ubuntu/run
+/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 kill all
+/home/ubuntu/.nvm/versions/node/v16.5.0/bin/yarn
+/home/ubuntu/.nvm/versions/node/v16.5.0/bin/yarn build
+/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 start dist/index.js
+/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 start dist/api
