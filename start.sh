b3ed0b4bbeb6f7326b2d9a2d2d16c742ccd3b879
jonathon <john@pokt.network>
2021-08-09 21:30:09 +0000

    appspec.yml

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 cd /home/ubuntu/run
-/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 kill all
-/home/ubuntu/.nvm/versions/node/v16.5.0/bin/yarn
-/home/ubuntu/.nvm/versions/node/v16.5.0/bin/yarn build
-/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 start dist/index.js
-/home/ubuntu/.nvm/versions/node/v16.5.0/bin/pm2 start dist/api
+pm2 kill
+yarn
+yarn build
+pm2 start dist/index.js
+pm2 start dist/api
