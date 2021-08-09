e0574ef3947cc1fc3cdd7af4dc3794838ca4242f
jonathon <john@pokt.network>
2021-08-09 21:30:09 +0000

    appspec.yml

--- init.sh
+++
@@ -1,5 +0,0 @@
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-#export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-#nvm install node
-#npm install pm2 yarn -g
