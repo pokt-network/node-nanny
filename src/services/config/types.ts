a9f70aa599d317902e8b8049282ef5bedaff7485
jonathon <john@pokt.network>
2021-08-11 18:10:23 +0000

    just some clean up

--- src/services/config/types.ts
+++ src/services/config/types.ts
@@ -4,15 +4,3 @@ export enum ConfigPrefix {
   EXTERNAL_ENDPOINT = "/pocket/monitoring/config/externalEndpoints",
   POCKET_NODES = "/pocket/monitoring/config/pocketNodes",
 }
-export enum ExternalEndPoints {
-  ETH = "/pocket/monitoring/config/externalEndpoints/eth",
-  BSC = "/pocket/monitoring/config/externalEndpoints/bsc",
-  FUS = "/pocket/monitoring/config/externalEndpoints/fus",
-  GOE = "/pocket/monitoring/config/externalEndpoints/goe",
-  KOV = "/pocket/monitoring/config/externalEndpoints/kov",
-  POL = "/pocket/monitoring/config/externalEndpoints/pol",
-  RIN = "/pocket/monitoring/config/externalEndpoints/rin",
-  ROP = "/pocket/monitoring/config/externalEndpoints/eth",
-  XDAI = "/pocket/monitoring/config/externalEndpoints/bsc",
-  AVA = "/pocket/monitoring/config/externalEndpoints/ava",
-}
