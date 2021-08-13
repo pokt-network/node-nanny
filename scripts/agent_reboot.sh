f763fd0dd6267677fe652c516d6796ab905362de
jonathon <john@pokt.network>
2021-08-13 02:33:14 +0000

    fix

--- scripts/agent_reboot.sh
+++ scripts/agent_reboot.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/node-deploy/shared
 stop=`docker stop $1`
 echo $stop
-sleep 5
+sleep 2
 echo "waiting for shutdown"
 compose=`docker-compose up -d`
 echo $stop $compose
--- src/services/alert/agent/index.ts
+++ src/services/alert/agent/index.ts
@@ -9,15 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post("/webhook/datadog/monitor/reboot", async ({ body }, res) => {
-  if (!body.name) {
-    return res.sendStatus(404);
-  }
   const { name } = body;
   try {
     const info = await alert.rebootNode(name);
     return res.json({ done: true, info });
   } catch (error) {
-    return res.sendStatus(500);
+    throw new Error(error);
   }
 });
 app.listen(port, () => {
