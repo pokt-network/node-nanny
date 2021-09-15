b7e301b9d74ca36477eee96191f584240f94039d
Jonathon Fritz <j@jonathonfritz.com>
2021-09-15 08:43:54 -0700

    HA proxy automation mvp (#62)
    * wip: add haproxy service to get status and stop/start servers
    
    * wip, events service added, alerts cleaned up, dd parse moved to dd service
    
    * wip: completed supporting services for events, ready for event logic
    
    * wip, first case covered
    
    * wip: add lb recovery
    
    * wip:core logic for event consumer
    
    * wip:core logic for event consumer
    
    * wip
    
    * file recovery from ec2
    
    * wip, add log service to dd, cross refernece logs to include container logs
    
    * wip
    
    * add getstatus to agent
    
    * wip:handle error and recovery case for when both nodes are out of sync
    
    * wip
    
    * ready for deployment
    
    Co-authored-by: Jonathon <john@pokt.network>
    Co-authored-by: Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>

---
+++ src/services/haproxy/service.ts
@@ -0,0 +1,68 @@
+import { exec } from "child_process";
+
+export class Service {
+  private async getCurrentStateByChainCommand(backend): Promise<string> {
+    return new Promise((resolve, reject) => {
+      exec(`echo "show servers state ${backend}" | nc -v localhost 9999`, (error, stdout) => {
+        if (error) {
+          reject(`error: ${error.message}`);
+        }
+        resolve(stdout);
+      });
+    });
+  }
+
+  private async disableServerCommand({ backend, host }) {
+    const cmd = `echo "disable server ${backend}/${host}" | nc -v localhost 9999`;
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout) => {
+        if (error) {
+          reject(`error: ${error.message}`);
+        }
+        resolve(stdout);
+      });
+    });
+  }
+  private async enableServerCommand({ backend, host }) {
+    const cmd = `echo "enable server ${backend}/${host}" | nc -v localhost 9999`;
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout) => {
+        if (error) {
+          reject(`error: ${error.message}`);
+        }
+        resolve(stdout);
+      });
+    });
+  }
+
+  async getStatus(backend) {
+    const raw = await this.getCurrentStateByChainCommand(backend);
+    const lines = raw.split("\n");
+    const [, , a, b] = lines;
+    const aStatusNum = Number(a.split(" ")[5]);
+    const bStatusNum = Number(b.split(" ")[5]);
+    const aStatus = aStatusNum === 2;
+    const bStatus = bStatusNum === 2;
+    const allOnline = aStatus === true && bStatus === true;
+    return { aStatus, bStatus, allOnline };
+  }
+
+  async disableServer({ backend, host }) {
+    const currentStatus = await this.getStatus(backend);
+    if (currentStatus.allOnline) {
+      return await this.disableServerCommand({ backend, host });
+    } else {
+      throw new Error(`one or more severs already offline ${currentStatus}`);
+    }
+  }
+
+  async enableServer({ backend, host }) {
+    await this.enableServerCommand({ backend, host });
+    const currentStatus = await this.getStatus(backend);
+    if (currentStatus.allOnline) {
+      return true;
+    } else {
+      throw new Error(`one or more severs are offline ${currentStatus}`);
+    }
+  }
+}
