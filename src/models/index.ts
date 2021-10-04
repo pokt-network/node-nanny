fce85f00832d8e177cdca32074bcdeaa40f9787a
Jonathon Fritz <j@jonathonfritz.com>
2021-10-04 15:32:31 -0700

    add db for config management  (#66)
    * wip, moving to other server
    
    * improved pocket support. added methods for automatic onboarding
    
    * fetch peers from db query
    
    Co-authored-by: Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>

---
+++ src/models/index.ts
@@ -0,0 +1,1 @@
+export { NodesModel, INode } from './nodes'
---
+++ src/models/nodes.ts
@@ -0,0 +1,98 @@
+import { Schema, model, Model, ObjectId } from 'mongoose'
+
+enum HostType {
+    AWS = "AWS",
+    OVH = "OVH"
+}
+
+interface IHosts {
+    name: string
+    internalIpaddress: string
+    internalHostName: string
+    externalHostName: string
+    awsInstanceId: string
+    loadbalancer: boolean
+    dockerHost: boolean
+    hostType: HostType
+}
+
+interface IPeer {
+    url: string
+    id: string
+}
+
+interface IChain {
+    chain: string
+    name: string
+    type: string
+}
+
+export interface INode {
+    id: string
+    backend: string
+    chain: IChain
+    container: string
+    externalNodes: string[]
+    host: IHosts
+    hostname: string
+    monitorId: string
+    online: boolean
+    peer: IPeer[]
+    port: number
+    threshold: number
+    url: string
+    variance: number
+}
+
+
+const chainSchema = new Schema<IChain>({
+    chain: String,
+    name: String,
+    type: String,
+})
+
+const hostsSchema = new Schema<IHosts>(
+    {
+        name: String,
+        internalIpaddress: String,
+        internalHostName: String,
+        externalHostName: String,
+        awsInstanceId: String,
+        loadbalancer: Boolean,
+        dockerHost: Boolean,
+        hostType: String
+
+    }
+)
+
+const peerSchema = new Schema({
+    url: String,
+    id: String
+})
+
+const nodesSchema = new Schema<INode>(
+    {
+        id: String,
+        backend: String,
+        chain: chainSchema,
+        container: String,
+        externalNodes: [String],
+        host: hostsSchema,
+        hostname: String,
+        monitorId: String,
+        online: Boolean,
+        peer: [peerSchema],
+        port: Number,
+        threshold: Number,
+        url: String,
+        variance: Number,
+    },
+    { collection: 'nodes' }
+)
+
+const NodesModel: Model<INode> = model(
+    'nodes',
+    nodesSchema
+)
+
+export { NodesModel }
