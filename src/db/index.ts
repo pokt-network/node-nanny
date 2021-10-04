9adcd6443a3868ebc2efd6b1b0f7b8caed045f4f
Jonathon Fritz <j@jonathonfritz.com>
2021-10-04 15:32:31 -0700

    add db for config management  (#66)
    * wip, moving to other server
    
    * improved pocket support. added methods for automatic onboarding
    
    * fetch peers from db query
    
    Co-authored-by: Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const connect = async (): Promise<void> => {
+    await mongoose.connect(process.env.MONGO_URI)
+}
+
+const disconnect = async (): Promise<void> => {
+    await mongoose.disconnect()
+}
+
+
+export { connect, disconnect }
