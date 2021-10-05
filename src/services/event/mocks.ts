018f094ceba7ef0080f85e1584cf030fbd3dd90d
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-10-05 21:45:42 +0000

    refactor events service for db, fix some issues with health service

--- src/services/event/mocks.ts
+++ src/services/event/mocks.ts
@@ -211,3 +211,30 @@ const generateMockEvents = () => {
 
 
 export default generateMockEvents()
+
+
+const newMock = {
+    msg: '%%%\n' +
+      '@webhook-events-production \n' +
+      'nodeId_615632b18b86f00010db487b\n' +
+      'event_NOT_SYNCHRONIZED"\n' +
+      '\n' +
+      '@webhook-events-production \n' +
+      'chain_pokt\n' +
+      'nodeId_615632b18b86f00010db487b\n' +
+      'event_NOT_SYNCHRONIZED_NOT_RESOLVED\n' +
+      '\n' +
+      'More than **4** log events matched in the last **5m** against the monitored query: **[status:error source:"/pocket/nodemonitoring/shared-2a/poltst"](https://app.datadoghq.eu/logs/analytics?query=status%3Aerror+source%3A%22%2Fpocket%2Fnodemonitoring%2Fshared-2a%2Fpoltst%22&agg_m=count&agg_t=count&agg_q=%40conditions&index=)** by **@conditions**\n' +
+      '\n' +
+      'The monitor was last triggered at Mon Oct 04 2021 22:40:23 UTC.\n' +
+      '\n' +
+      '- - -\n' +
+      '\n' +
+      '[[Monitor Status](https://app.datadoghq.eu/monitors/2528963?to_ts=1633387223000&group=%40conditions%3ANOT_SYNCHRONIZED&from_ts=1633386323000)] · [[Edit Monitor](https://app.datadoghq.eu/monitors#2528963/edit)] · [[Related Logs](https://app.datadoghq.eu/logs/analytics?index=%2A&to_ts=1633387223000&agg_t=count&agg_m=count&agg_q=%40conditions&from_ts=1633386323000&live=false&query=status%3Aerror+source%3A%22%2Fpocket%2Fnodemonitoring%2Fshared-2a%2Fpoltst%22)]',
+    id: '2528963',
+    transition: 'Re-Triggered',
+    type: 'error',
+    title: '[Re-Triggered on {@conditions:NOT_SYNCHRONIZED}] SHARED-2A/POLTST',
+    status: '',
+    link: 'https://app.datadoghq.eu/event/event?id=6192890666631275067'
+  }
