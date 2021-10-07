5860723b09d59d163544edad5a5fc6f0a77d3c75
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-10-07 22:57:09 +0000

    add solana and algorand support, bug fixes, add test discord webhooks

--- src/services/health/service.ts
+++ src/services/health/service.ts
@@ -160,7 +160,6 @@ export class Service {
     if (last - secondLast < variance) {
       return sorted.pop();
     } else {
-      console.error(`block height not within variance ${readings}`);
       return sorted.pop();
     }
   }
@@ -183,7 +182,6 @@ export class Service {
     return new Promise((resolve, reject) => {
       exec(`nc -vz -q 2 ${host} ${port}`, (error, stdout, stderr) => {
         if (error) {
-          console.error(`nc ${error}`);
           reject(`error: ${error.message}`);
         }
         if (stderr) {
@@ -217,10 +215,30 @@ export class Service {
       }))
   }
 
-  private async getEthNodeHealth(node: INode): Promise<{}> {
-    const { chain, url, externalNodes, variance, threshold, host, id } = node
-
+  private async getEVMNodeHealth(node: INode): Promise<{}> {
+    const { chain, url, externalNodes, variance, threshold, host, id, port } = node
     const name = `${host.name}/${chain.name}`
+
+    //Check if node is online and RPC up
+    const isNodeListening = await this.isNodeListening({ host: host.internalIpaddress, port });
+
+    if (!isNodeListening) {
+      return {
+        name,
+        status: ErrorStatus.ERROR,
+        conditions: ErrorConditions.OFFLINE,
+      }
+    }
+
+    const isRpcResponding = await this.isRpcResponding({ url, chain: chain.name })
+
+    if (!isRpcResponding) {
+      return {
+        name,
+        status: ErrorStatus.ERROR,
+        conditions: ErrorConditions.NO_RESPONSE,
+      }
+    }
 
     let peers: INode[] = await NodesModel.find({ "chain.name": chain.name, _id: { $ne: id } }).exec()
 
@@ -286,7 +304,80 @@ export class Service {
     }
   }
 
-  async getPocketNodeHealth({ hostname, port, variance, id }: INode) {
+  async getSolHealth({ url, name }) {
+    try {
+      const { data } = await this.rpc.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "getHealth",
+      });
+
+      const { result } = data;
+
+      if (result == 'ok') {
+        return {
+          name,
+          conditions: ErrorConditions.HEALTHY,
+          status: ErrorStatus.OK,
+          health: result,
+        };
+      } else {
+        return {
+          name,
+          conditions: ErrorConditions.NOT_SYNCHRONIZED,
+          status: ErrorStatus.ERROR,
+          health: result,
+        };
+      }
+    } catch (error) {
+      throw new Error(`could not contact blockchain node ${error} ${url}`);
+    }
+  }
+
+  async getAlgorandHealth({ url, name }) {
+    try {
+      const { data, status } = await this.rpc.get(`${url}/health`)
+      if (status == 200) {
+        return {
+          name,
+          conditions: ErrorConditions.HEALTHY,
+          status: ErrorStatus.OK
+        };
+      } else {
+        return {
+          name,
+          conditions: ErrorConditions.NOT_SYNCHRONIZED,
+          status: ErrorStatus.ERROR,
+          health: data ? data.result : [],
+        };
+      }
+    } catch (error) {
+      throw new Error(`could not contact blockchain node ${error} ${url}`);
+    }
+  }
+
+  async getPocketNodeHealth({ hostname, port, variance, id, host }: INode) {
+    //check if responding
+    const isNodeListening = await this.isNodeListening({ host: host.internalIpaddress, port });
+    if (!isNodeListening) {
+      return {
+        name: hostname,
+        status: ErrorStatus.ERROR,
+        conditions: ErrorConditions.OFFLINE,
+      }
+    }
+
+    const { height: isRpcResponding } = await this.getPocketHeight(`https://${hostname}:${port}`)
+
+    if (isRpcResponding === 0) {
+      return {
+        name: hostname,
+        status: ErrorStatus.ERROR,
+        conditions: ErrorConditions.NO_RESPONSE,
+      }
+    }
+
+
     // get list of reference nodes
     const referenceNodes: INode[] = await NodesModel.find({
       "chain.type": "POKT", _id: { $ne: id }
@@ -297,7 +388,7 @@ export class Service {
     const [highest] = pocketheight.map(({ height }) => height).sort().slice(-1)
     const { height } = await this.getPocketHeight(`https://${hostname}:${port}`)
 
-    const notSynched  = (Number(highest) - Number(height)) > variance
+    const notSynched = (Number(highest) - Number(height)) > variance
 
     if (notSynched) {
       return {
@@ -328,48 +419,25 @@ export class Service {
   async getNodeHealth(nodes: INode[]) {
     const results = []
     for (const node of nodes) {
-      const { chain, host, hostname, port, url } = node;
-      //Check if node is online and RPC up
-      const isNodeListening = await this.isNodeListening({ host: host.internalIpaddress, port });
-      let isRpcResponding
-
-      if (node.chain.type == SupportedBlockChainTypes.POKT) {
-        const { height } = await this.getPocketHeight(`https://${hostname}:${port}`)
-        isRpcResponding = (height !== 0)
-      } else {
-        isRpcResponding = await this.isRpcResponding({ url, chain: chain.name })
-      }
-
-      if (!isNodeListening) {
-        results.push({
-          name: hostname,
-          status: ErrorStatus.ERROR,
-          conditions: ErrorConditions.OFFLINE,
-        })
-      }
-
-      if (isNodeListening && !isRpcResponding) {
-        results.push({
-          name: hostname,
-          status: ErrorStatus.ERROR,
-          conditions: ErrorConditions.NO_RESPONSE,
-        })
-      }
-
+      const { chain, host, url } = node;
       //Check Health
-      if (isNodeListening && isRpcResponding) {
-        if (chain.type == SupportedBlockChainTypes.POKT) {
-          results.push(await this.getPocketNodeHealth(node))
-        }
-        if (chain.type == SupportedBlockChainTypes.ETH) {
-          results.push(await this.getEthNodeHealth(node))
-        }
-        if (chain.type == SupportedBlockChainTypes.AVA) {
-          results.push(await this.getAvaHealth({ name: `${host.name}/${chain.name}`, url: url }))
-        }
-        if (chain.type == SupportedBlockChainTypes.HEI) {
-          results.push(await this.getHeimdallHealth({ name: `${host.name}/${chain.name}`, url: url }))
-        }
+      if (chain.type == SupportedBlockChainTypes.POKT) {
+        results.push(await this.getPocketNodeHealth(node))
+      }
+      if (chain.type == SupportedBlockChainTypes.ETH) {
+        results.push(await this.getEVMNodeHealth(node))
+      }
+      if (chain.type == SupportedBlockChainTypes.AVA) {
+        results.push(await this.getAvaHealth({ name: `${host.name}/${chain.name}`, url: url }))
+      }
+      if (chain.type == SupportedBlockChainTypes.HEI) {
+        results.push(await this.getHeimdallHealth({ name: `${host.name}/${chain.name}`, url: url }))
+      }
+      if (chain.type == SupportedBlockChainTypes.SOL) {
+        results.push(await this.getSolHealth({ name: `${host.name}/${chain.name}`, url: url }))
+      }
+      if (chain.type == SupportedBlockChainTypes.ALG) {
+        results.push(await this.getAlgorandHealth({ name: `${host.name}/${chain.name}`, url: url }))
       }
     }
     return results
