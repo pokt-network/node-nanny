68ee04f8c9a6220503bfd13e64054a7c90cbacf8
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-10-21 19:06:59 +0000

    some fixes

--- src/fix.ts
+++ src/fix.ts
@@ -1,17 +1,12 @@
 import { connect } from "./db";
 import { INode, NodesModel, IOracle, OraclesModel } from "./models";
+import { Service } from "../src/services/datadog"
+
+const dd = new Service()
 
 const fix = async () => {
     await connect()
-    const allNodes = await NodesModel.find({})
-
-    for (const node of allNodes) {
-        const res = await NodesModel.findOneAndUpdate({ _id: node._id }, { reboot: false })
-        console.log(res)
-    }
     return
 }
-
-
 
 fix()
--- src/services/reboot/service.ts
+++ src/services/reboot/service.ts
@@ -1,10 +1,24 @@
 import { exec } from "child_process";
 import path from "path";
 
+enum NodeTypes {
+  DATA = "DATA",
+  POKT = "POKT"
+}
+
 export class Service {
-  rebootDockerContainerFromAgent(name) {
-    const script = path.resolve(__dirname, "../../../scripts/agent_reboot.sh");
-    const cmd = `sh ${script} ${name}`
+  rebootDockerContainerFromAgent({ name, type, nginx, poktType }) {
+    let cmd, script
+    if (type == NodeTypes.DATA) {
+      script = path.resolve(__dirname, "../../../scripts/agent_reboot.sh");
+      cmd = `sh ${script} ${name} ${nginx} ${poktType}`
+    }
+
+    if (type === NodeTypes.POKT) {
+      script = path.resolve(__dirname, "../../../scripts/agent_reboot_pokt.sh");
+      cmd = `sh ${script} ${name}`
+    }
+
     return new Promise((resolve, reject) => {
       exec(cmd, (error, stdout, stderr) => {
         if (error) {
