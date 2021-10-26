8186d5e4c7ed610c918b02852afaa7a72b783ef3
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-10-26 17:58:14 +0000

    wip

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 import { Reboot, HAProxy } from "../services";
 
 config();
+
 const reboot = new Reboot();
 const lb = new HAProxy();
 const app = express();
@@ -15,7 +16,13 @@ app.use(express.json());
 app.post("/webhook/docker/reboot", async ({ body }, res) => {
   const { name, type, compose, nginx, poktType } = body;
   try {
-    const status = await reboot.rebootDockerContainerFromAgent({ name, type, compose, nginx, poktType });
+    const status = await reboot.rebootDockerContainerFromAgent({
+      name,
+      type,
+      compose,
+      nginx,
+      poktType,
+    });
     return res.json({ reboot: status });
   } catch (error) {
     throw new Error(error);
@@ -24,8 +31,6 @@ app.post("/webhook/docker/reboot", async ({ body }, res) => {
 
 app.post("/webhook/lb/disable", async ({ body }, res) => {
   const { backend, host } = body;
-
-  console.log({ backend, host })
   try {
     const status = await lb.disableServer({ backend, host });
     return res.json({ status });
@@ -44,19 +49,15 @@ app.post("/webhook/lb/enable", async ({ body }, res) => {
   }
 });
 
-
 app.post("/webhook/lb/status", async ({ body }, res) => {
   const { backend } = body;
-
-  console.log(body)
   try {
-    const status = await lb.getStatus(backend)
+    const status = await lb.getStatus(backend);
     return res.json({ status });
   } catch (error) {
     return res.status(500).send(error);
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Webhook api listening at http://localhost:${port}`);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connect = async (): Promise<void> => {
-    await mongoose.connect(process.env.MONGO_URI)
-}
+  await mongoose.connect(process.env.MONGO_URI);
+};
 
 const disconnect = async (): Promise<void> => {
-    await mongoose.disconnect()
-}
+  await mongoose.disconnect();
+};
 
-export { connect, disconnect }
+export { connect, disconnect };
