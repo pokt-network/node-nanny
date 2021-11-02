ddf411c75a308cfcd525ee98bef987e6a1e56ce6
JonathonJulian <john@pokt.network>
2021-11-02 23:06:51 +0000

    fix: haproxy status

--- src/fix.ts
+++ src/fix.ts
@@ -1,11 +1,14 @@
 import { connect } from "./db";
 import { INode, NodesModel, IOracle, OraclesModel } from "./models";
-import { Service } from "../src/services/datadog";
-
-const dd = new Service();
+import { Retool, DataDog } from "./services";
+const dd = new DataDog();
+const retool = new Retool();
 
 const fix = async () => {
   await connect();
+
+ //const status =  await retool.getHaProxyStatus('61563127857d870012a409bd')
+ //console.log(status);
 
   return;
 };
