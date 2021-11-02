a334b8b0e39c48cad297bcf8dd9276056db574ba
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-11-02 21:54:44 +0000

    automation support for <2 nodes per chain

--- src/services/haproxy/haproxy.test.ts
+++ src/services/haproxy/haproxy.test.ts
@@ -1,5 +1,13 @@
 import { Service } from "./service";
 
+const service = new Service();
+
 test("", async () => {
+
+  const status = await service.getStatus({backend: "ethmainnet", server: "2a"});
+  console.log(status);
+
+
+
   expect(3).toBe(3);
 });
