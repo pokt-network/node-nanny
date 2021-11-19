a857e6389319ba273909148f015da57adb2ad3ba
Ubuntu <ubuntu@ip-10-0-0-85.us-east-2.compute.internal>
2021-11-19 00:36:40 +0000

    service restart

--- src/services/reboot/service.ts
+++ src/services/reboot/service.ts
@@ -3,26 +3,38 @@ import path from "path";
 
 enum NodeTypes {
   DATA = "data",
-  POKT = "pokt"
+  POKT = "pokt",
 }
 
 export class Service {
   rebootDockerContainerFromAgent({ name, type, compose, nginx, poktType }) {
-    console.log(name)
-    let cmd, script
+    let cmd, script;
     if (type == NodeTypes.DATA) {
       script = path.resolve(__dirname, "../../../scripts/agent_reboot.sh");
-      cmd = `sh ${script} ${name} ${compose}`
+      cmd = `sh ${script} ${name} ${compose}`;
     }
     if (type === NodeTypes.POKT) {
       script = path.resolve(__dirname, "../../../scripts/agent_reboot_pokt.sh");
-      cmd = `sh ${script} ${name} ${nginx} ${poktType}`
+      cmd = `sh ${script} ${name} ${nginx} ${poktType}`;
     }
 
     return new Promise((resolve, reject) => {
       exec(cmd, (error, stdout, stderr) => {
         if (error) {
-          console.error(`nc ${error}`);
+          reject(`error: ${error.message}`);
+        }
+        if (stderr) {
+          resolve(stderr);
+        }
+        resolve(stdout);
+      });
+    });
+  }
+
+  restartService(service) {
+    return new Promise((resolve, reject) => {
+      exec(`sudo service ${service} restart`, (error, stdout, stderr) => {
+        if (error) {
           reject(`error: ${error.message}`);
         }
         if (stderr) {
