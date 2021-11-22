8c0447729f3c52b676914968985018e8d600f8b5
Jonathon <jonathon@pokt.network>
2021-11-22 22:31:29 +0000

    some cleanup

--- src/services/haproxy/haproxy.test.ts
+++ src/services/haproxy/haproxy.test.ts
@@ -1,12 +1,6 @@
 import { Service } from "./service";
-
 const service = new Service();
 
 test("", async () => {
-
-  const status = await service.getServerCount({backend: "bscmainnet"});
-  console.log(status);
-
-
   expect(3).toBe(3);
 });
