8672c80811924bbeda86b95c5d408b3a1b38fbe3
Jonathon <jonathon@pokt.network>
2021-11-25 23:10:14 +0000

    add pub/sub

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Redis from "ioredis";
 import { Health, Log } from "./services";
 import { NodesModel } from "./models";
 import { connect } from "./db";
@@ -7,21 +8,27 @@ import "./tracer";
 export class App {
   private log: Log;
   private health: Health;
+  private redis: Redis;
   constructor() {
     this.health = new Health();
     this.log = new Log();
+    this.redis = new Redis();
   }
 
   async main() {
     await connect();
-    const nodes = await NodesModel.find({
-
-    }).exec();
+    const nodes = await NodesModel.find({}).exec();
     for (const node of nodes) {
       setInterval(async () => {
         node.id = node._id;
         const message = await this.health.getNodeHealth(node);
         if (message && message.status) {
+          if (message.status === HealthTypes.ErrorStatus.ERROR) {
+            await this.redis.publish(
+              "send-error-event",
+              JSON.stringify({ ...message, id: node.id }),
+            );
+          }
           console.log({ message });
           return await this.log.writeLogtoDB({
             message,
