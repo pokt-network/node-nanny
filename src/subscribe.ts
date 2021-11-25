c892e7ce6fa508a3be40778a07c9432811809943
Jonathon <jonathon@pokt.network>
2021-11-25 23:10:14 +0000

    add pub/sub

---
+++ src/subscribe.ts
@@ -0,0 +1,16 @@
+import Redis from "ioredis";
+const redis = new Redis();
+
+const main = () => {
+  redis.subscribe("send-error-event", (err, count) => {
+    if (err) console.error(err.message);
+    console.log(`Subscribed to ${count} channels.`);
+  });
+
+  redis.on("message", (channel, message) => {
+    console.log(`Received message from ${channel} channel.`);
+    console.log(JSON.parse(message));
+  });
+};
+
+main();
