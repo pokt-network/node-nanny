d5b12489c1075ab75422094d7cd386fa14e2252e
Jonathon <jonathon@pokt.network>
2021-11-26 00:52:18 +0000

    wip, track error count

--- src/subscribe.ts
+++ src/subscribe.ts
@@ -6,6 +6,11 @@ const main = () => {
     if (err) console.error(err.message);
     console.log(`Subscribed to ${count} channels.`);
   });
+  
+  redis.subscribe("send-resolved-event", (err, count) => {
+    if (err) console.error(err.message);
+    console.log(`Subscribed to ${count} channels.`);
+  });
 
   redis.on("message", (channel, message) => {
     console.log(`Received message from ${channel} channel.`);
