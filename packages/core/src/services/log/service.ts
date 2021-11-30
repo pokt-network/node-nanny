3e07ac99872e1479b4a44f05b1bdc6178d70983a
Jonathon <jonathon@pokt.network>
2021-11-30 22:05:33 +0000

    config options to support either dd or mongo log transport

--- packages/core/src/services/log/service.ts
+++ packages/core/src/services/log/service.ts
@@ -1,30 +1,61 @@
 import { createLogger, format, transports, Logger } from "winston";
-import { LogsModel } from "../../models";
+import "winston-mongodb";
+
+//move types to shared
+type Config = {
+  logger: LoggerOptions;
+  event: EventOptions;
+};
+
+enum LoggerOptions {
+  MONGODB = "mongodb",
+  DATADOG = "datadog",
+}
+enum EventOptions {
+  REDIS = "redis",
+  DATADOG = "datadog",
+}
 
 export class Service {
-  init(name: string): Logger {
+  private config: Config;
+  constructor(config) {
+    this.config = config;
+  }
+
+  init({ name, id }: { name: string; id: string }): Logger {
+    const transport = {
+      mongodb: new transports.MongoDB({
+        db: process.env.MONGO_URI_LOGS,
+        expireAfterSeconds: 60 * 60 * 24,
+        label: id,
+      }),
+      datadog: new transports.Http({
+        host: "http-intake.logs.datadoghq.eu",
+        path: `/api/v2/logs?dd-api-key=${process.env.DD_API_KEY}&ddsource=nodejs&service=${name}`,
+        ssl: true,
+      }),
+    }[ this.config.logger];
+
     return createLogger({
       level: "info",
       exitOnError: false,
       format: format.json(),
-      transports: [
-        new transports.Http({
-          host: "http-intake.logs.datadoghq.eu",
-          path: `/api/v2/logs?dd-api-key=${process.env.DD_API_KEY}&ddsource=nodejs&service=${name}`,
-          ssl: true,
-        }),
-      ],
+      transports: [transport],
     });
   }
 
-  async write({ name, message, level }: { name: string; message: string; level: string }) {
-    const logger = this.init(name);
+  async write({
+    name,
+    message,
+    level,
+    id,
+  }: {
+    name: string;
+    message: string;
+    level: string;
+    id: string;
+  }) {
+    const logger = this.init({ name, id });
     return await logger.log(level, message);
-  }
-
-  async writeLogtoDB({ nodeId, message }) {
-    const data = { ...message, nodeId, createdAt: new Date() };
-    const log = new LogsModel(data);
-    return await log.save();
   }
 }
