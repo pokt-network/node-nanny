da52744426c69e6b34e095cb5b03e317ad679bdf
Jonathon <jonathon@pokt.network>
2021-12-03 00:39:11 +0000

    wip

--- packages/ui/src/views/index.ts
+++ packages/ui/src/views/index.ts
@@ -3,3 +3,5 @@ export { View as Hosts } from "./Hosts";
 export { View as Nodes } from "./Nodes";
 export { View as Logs } from "./Logs";
 export { View as Settings } from "./Settings";
+export { View as Oracles } from "./Oracles";
+export { View as Home } from "./Home";
