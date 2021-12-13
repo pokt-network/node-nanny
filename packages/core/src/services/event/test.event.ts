8ffece20a2a11dd9d5dc119083285ace1c5c0f6b
Jonathon <jonathon@pokt.network>
2021-12-13 19:06:29 +0000

    wip

--- packages/core/src/services/event/test.event.ts
+++ packages/core/src/services/event/test.event.ts
@@ -1,4 +1,4 @@
-import { Service } from "./service";
+import { Service } from "./service/datadog";
 import { config } from "dotenv";
 import { wait } from "../../utils";
 import { connect } from "../../db";
