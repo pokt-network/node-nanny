e5cb446403250f153be9e37d11d450cb96737cbe
Jonathon <jonathon@pokt.network>
2021-12-30 03:18:19 +0000

    wip redis event consumer

---
+++ packages/core/src/services/event/service/redis/index.ts
@@ -0,0 +1,4 @@
+import { Service as Triggered } from "./trigger";
+import { Service as ReTriggered } from "./retrigger";
+import { Service as Resolved } from "./resolved";
+export default { Triggered, ReTriggered, Resolved };
