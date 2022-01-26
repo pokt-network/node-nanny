18cc40802c920d5f2b137fc210a2af8676c04ef3
Ubuntu <ubuntu@ip-172-31-9-193.eu-central-1.compute.internal>
2022-01-26 00:48:40 +0000

    wip: moving dev enviroment to home

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,3 +1,3 @@
-export MONGO_URI="mongodb+srv://jfritz:Y755TZJBUU8vEP3a@cluster0.fdewr.mongodb.net/node-monitoring?retryWrites=true&w=majority"
+export MONGO_URI="mongodb+srv"
 export MONITOR_EVENT=redis
 export MONITOR_LOGGER=mongodb
