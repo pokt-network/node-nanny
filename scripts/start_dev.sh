d383994e056d95c3a6479a330be57fc96c714eb3
JonathonJulian <john@pokt.network>
2022-02-13 11:18:21 -0800

    wip

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,7 +1,8 @@
-cd /home/ubuntu/node-nanny/packages/core && pnpm run build:watch
-cd /home/ubuntu/node-nanny/packages/monitor && pnpm run build:watch
-cd /home/ubuntu/node-nanny/packages/event-consumer && pnpm run build:watch
+cd /home/jonathon/projects/node-nanny/packages/core && pnpm run build:watch
+cd /home/jonathon/projects/node-nanny/packages/monitor && pnpm run build:watch
+cd /home/jonathon/projects/node-nanny/packages/event-consumer && pnpm run build:watch
+cd /home/jonathon/projects/node-nanny/packages/api && pnpm run build:watch
 
-
-cd /home/ubuntu/node-nanny/packages/monitor && nodemon dist
-cd /home/ubuntu/node-nanny/packages/event-consumer && nodemon dist
+cd /home/jonathon/projects/node-nanny/packages/api && nodemon dist/graphql
+cd /home/jonathon/projects/node-nanny/packages/monitor && nodemon dist
+cd /home/jonathon/projects/node-nanny/packages/event-consumer && nodemon dist
