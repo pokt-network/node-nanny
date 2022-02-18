abfae4f025b94a36a919dfa45a547bf0bff62242
Jonathon <jonathon@pokt.network>
2022-02-18 19:23:29 +0000

    Ran Prettier over whole repo.

---
+++ packages/ui/src/views/Chains/ChainsTable.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Table } from "../../components";
+
+interface ViewProps {
+  children?: React.ReactNode;
+}
+
+export function ChainsTable(props: ViewProps) {
+  return <Table />;
+}
