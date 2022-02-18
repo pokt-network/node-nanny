59a6298b0ba2540bbbb39767d46f2f3996cb426a
Jonathon <jonathon@pokt.network>
2022-02-18 22:16:05 +0000

    Created reusable Table component with Sort By functionality

--- packages/ui/src/components/Table.tsx
+++ packages/ui/src/components/Table.tsx
@@ -1,35 +1,127 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
 import MUITable from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import Paper from "@mui/material/Paper";
 
-export function Table({ rows }: { rows: any[] }) {
-  console.log("INSIDE TABLE", rows);
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface EnhancedTableProps {
+  rows: any[];
+  onRequestSort: (event: React.MouseEvent<unknown>, property: any) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { rows, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: any) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
+  const headCells = Object.keys(rows[0])
+    .filter((value) => value !== "id")
+    .map((column) => ({ id: column, label: column[0].toUpperCase() + column.slice(1) }));
+
   return (
-    <TableContainer component={Paper}>
-      <MUITable sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Chain</TableCell>
-            <TableCell align="right">Type</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.chain}</TableCell>
-              <TableCell align="right">{row.type}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </MUITable>
-    </TableContainer>
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell, i) => (
+          <TableCell
+            key={headCell.id}
+            align={!i ? "left" : "right"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface TableProps {
+  rows: any[];
+  height?: number;
+}
+
+export function Table({ rows, height }: TableProps) {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState("calories");
+
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: any) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: height || 600 }}>
+          <MUITable stickyHeader sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={"small"}>
+            <EnhancedTableHead
+              rows={rows}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {rows
+                .slice()
+                .sort(getComparator(order, orderBy))
+                .map((row: any, index: any) => {
+                  return (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      {Object.entries(row)
+                        .filter(([key]) => key !== "id")
+                        .map(([_, value], i) => {
+                          console.log({ value });
+                          return (
+                            <TableCell align={!i ? "left" : "right"}>{value as string}</TableCell>
+                          );
+                        })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </MUITable>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 }
