16a7908d186c52242ee9b68f45912c31d92d80dd
Jonathon <jonathon@pokt.network>
2022-02-18 22:16:05 +0000

    Created reusable Table component with Sort By functionality

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,17 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 const client = new ApolloClient({
   uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root"),
