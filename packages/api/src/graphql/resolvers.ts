b259e2dd5f1b4f1b94feb884f70940b4a95d7db1
Jonathon <jonathon@pokt.network>
2022-02-19 03:52:27 +0000

    EOD Friday commit - refactored api graphql, added Table component, pages in UI.

---
+++ packages/api/src/graphql/resolvers.ts
@@ -0,0 +1,58 @@
+import {
+  NodesModel,
+  HostsModel,
+  ChainsModel,
+  OraclesModel,
+  LogsModel,
+  WebhookModel,
+} from "@pokt-foundation/node-monitoring-core/dist/models";
+
+const resolvers = {
+  Query: {
+    chains: async () => await ChainsModel.find({}).exec(),
+    hosts: async (_, { loadBalancer }) => {
+      const query = loadBalancer === true ? { loadBalancer } : {};
+      return await HostsModel.find(query).sort({ name: 1 }).exec();
+    },
+    logs: async ({ id }) => await LogsModel.find({ label: id }).exec(),
+    nodes: async () => await NodesModel.find({}).exec(),
+    oracles: async () => await OraclesModel.find({}).exec(),
+    webhooks: async () => await WebhookModel.find({}).exec(),
+  },
+
+  Mutation: {
+    createChain: async (_, chain) => {
+      return await ChainsModel.create(chain);
+    },
+    createHost: async (_, host) => {
+      return await HostsModel.create(host);
+    },
+    createNode: async (_, { input }) => {
+      return await NodesModel.create(input);
+    },
+    createOracle: async (_, { chain, url }) => {
+      const doesExist = await OraclesModel.findOne({ chain }).exec();
+      if (!doesExist) {
+        return await OraclesModel.create({ chain, urls: [url] });
+      }
+      return await OraclesModel.findOneAndUpdate({ chain }, { $push: { urls: url } }).exec();
+    },
+    createWebhook: async (_, webhook) => {
+      return await WebhookModel.create(webhook);
+    },
+  },
+
+  Chain: {
+    id(chain: any) {
+      return chain._id;
+    },
+  },
+
+  Host: {
+    id(host: any) {
+      return host._id;
+    },
+  },
+};
+
+export default resolvers;
--- packages/ui/src/pages/index.ts
+++ packages/ui/src/pages/index.ts
@@ -1,8 +1,8 @@
-export { View as Chains } from "./Chains";
-export { View as Hosts } from "./Hosts";
+export { Chains } from "./Chains";
+export { Hosts } from "./Hosts";
 export { View as Nodes } from "./Nodes";
 export { View as Logs } from "./Logs";
 export { View as Settings } from "./Settings";
 export { View as Oracles } from "./Oracles";
 export { View as Home } from "./Home";
-export {View as Webhooks} from "./Webhooks";
+export { View as Webhooks } from "./Webhooks";
