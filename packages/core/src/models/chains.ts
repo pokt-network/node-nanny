2b496fc0cc8a6a01e69c1af890063e296a2de57e
pascalvl <pascal@coinmiles.io>
2022-02-22 15:29:18 -0800

    Moved variance field to chain model from node. Added unique keys & timestamps to mongo models.

--- packages/core/src/models/chains.ts
+++ packages/core/src/models/chains.ts
@@ -4,12 +4,17 @@ export interface IChain {
   chain: string;
   name: string;
   type: string;
+  variance: number;
 }
 
-export const chainSchema = new Schema<IChain>({
-  chain: String,
-  name: String,
-  type: String,
-});
+export const chainSchema = new Schema<IChain>(
+  {
+    name: { type: String, unique: true },
+    chain: String,
+    type: String,
+    variance: Number,
+  },
+  { timestamps: true },
+);
 
 export const ChainsModel: Model<IChain> = model("chains", chainSchema);
