46c055b8e333c7afb4fe3838dea7cd698b011856
Jonathon <jonathon@pokt.network>
2022-02-25 18:41:32 +0000

    Confirmed types for reboot return and added central place for enum.

--- packages/ui/src/pages/Nodes/Nodes.tsx
+++ packages/ui/src/pages/Nodes/Nodes.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import { Table } from "components";
@@ -9,39 +9,46 @@ import { NodeStatus } from "./NodeStatus";
 
 export function Nodes() {
   const [selectedNode, setSelectedNode] = useState<INode | undefined>(undefined);
-  const { data, error } = useQuery<{ nodes: INode[] }>(GET_ALL_NODES);
+  const { data, error, loading } = useQuery<{ nodes: INode[] }>(GET_ALL_NODES);
 
-  if (error) console.log("ERROR LOADING NODES", error);
+  useEffect(() => data?.nodes[0] && setSelectedNode(data!.nodes[0]), [data]);
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "16px",
-      }}
-    >
+  if (loading && !selectedNode) return <>Loading...</>;
+  if (error) return <>Error! ${error.message}</>;
+
+  if (data && selectedNode) {
+    return (
       <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
-          width: "60%",
-          marginBottom: "16px",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "16px",
         }}
       >
-        <NodesForm />
-        <NodeStatus selectedNode={selectedNode!} />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "60%",
+            marginBottom: "16px",
+          }}
+        >
+          <NodesForm />
+          <NodeStatus selectedNode={selectedNode!} />
+        </div>
+        {data && (
+          <Table
+            paginate
+            rows={data.nodes}
+            selectedRow={selectedNode?.id}
+            onSelectRow={setSelectedNode}
+          />
+        )}
       </div>
-      {data && (
-        <Table
-          paginate
-          rows={data.nodes}
-          selectedRow={selectedNode?.id}
-          onSelectRow={setSelectedNode}
-        />
-      )}
-    </div>
-  );
+    );
+  }
+
+  return <></>;
 }
