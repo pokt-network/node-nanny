b201d7ff9493affd20202a523f0159d92f964ec3
Jonathon <jonathon@pokt.network>
2022-03-02 21:20:51 +0000

    Created Locations collection and configured GraphQL Api to fetch string field for Hosts.

--- packages/core/src/models/index.ts
+++ packages/core/src/models/index.ts
@@ -2,5 +2,6 @@ export { HostsModel, IHost } from "./hosts";
 export { NodesModel, INode } from "./nodes";
 export { ChainsModel, IChain } from "./chains";
 export { OraclesModel, IOracle } from "./oracles";
+export { LocationsModel, ILocation } from "./locations";
 export { LogsModel, ILog } from "./logs";
 export { WebhookModel, IWebhook } from "./webhooks";
