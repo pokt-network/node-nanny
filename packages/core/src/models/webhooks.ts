0e5411c72306cbb5e392b7ef492a104dff9250ee
Jonathon <jonathon@pokt.network>
2022-03-04 02:14:00 +0000

    Updated host to have location as required field.

--- packages/core/src/models/webhooks.ts
+++ packages/core/src/models/webhooks.ts
@@ -2,17 +2,19 @@ import { Schema, model, Model } from "mongoose";
 
 export interface IWebhook {
   chain: string;
-  location: string;
   url: string;
+  location: string;
 }
 
 const webhookSchema = new Schema<IWebhook>(
   {
-    chain: { type: String, required: true, unique: true },
-    url: { type: String, required: true },
-    location: String,
+    chain: { type: String, required: true },
+    url: { type: String, required: true, unique: true },
+    location: { type: String, required: true },
   },
   { timestamps: true },
 );
+
+webhookSchema.index({ chain: 1, location: 1 }, { unique: true });
 
 export const WebhookModel: Model<IWebhook> = model("webhooks", webhookSchema);
