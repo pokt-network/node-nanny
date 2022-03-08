967195b712539ca24bbd4080974519e6286411ae
commoddity <pascal@pokt.network>
2022-03-08 05:25:20 +0000

    Set up chains/oracles Lambda and updater script running on a one hour interval via PM2.

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -7,3 +7,12 @@ export const wait = async (ms: number): Promise<void> => {
 export const hexToDec = (resultString: number | string): number => {
   return typeof resultString === "number" ? resultString : parseInt(resultString, 16);
 };
+
+export const getTimestamp = (): string => {
+  const pad = (n: number, s = 2) => `${new Array(s).fill(0)}${n}`.slice(-s);
+  const d = new Date();
+
+  return `${pad(d.getFullYear(), 4)}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(
+    d.getHours(),
+  )}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+};
