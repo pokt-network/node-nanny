6073b73d0811db4026070b2bcfd40fd427ad151a
commoddity <pascal@pokt.network>
2022-03-08 05:25:20 +0000

    Set up chains/oracles Lambda and updater script running on a one hour interval via PM2.

--- node-nanny.config.js
+++ node-nanny.config.js
@@ -17,6 +17,11 @@ module.exports = {
       script: "./packages/monitor/dist/index.js",
     },
     {
+      name: "updater",
+      script: "./packages/core/dist/jobs/updater.js",
+      restart_delay: 1000 * 60 * 60,
+    },
+    {
       name: "ui",
       cwd: "./packages/ui",
       script: "npm",
