a3fae9d8073e3c6aef5ff0522e3679ec81695656
commoddity <pascal@pokt.network>
2022-03-09 20:01:23 +0000

    Finished setting up base service for event consumer handler and refactored.

--- packages/core/src/services/event/service/redis/base-service.ts
+++ packages/core/src/services/event/service/redis/base-service.ts
@@ -1,34 +1,17 @@
 import axios, { AxiosInstance } from "axios";
 
 import { Alert } from "../../..";
-import { AlertTypes } from "../../../../types";
-import { LoadBalancerStatus, LoadBalancer } from "../../types";
+import { LoadBalancerStatus } from "../../types";
 import { IRotationParams } from "./types";
-import { HostsModel, NodesModel, INode } from "../../../../models";
+import { NodesModel, INode } from "../../../../models";
 
 export default class Service {
   private agent: AxiosInstance;
-  // DEV NOTE -> Do we need this?
-  sendErrorChannel: AlertTypes.ISendAlert;
-  sendError: AlertTypes.ISendAlert;
-  sendInfo: AlertTypes.ISendAlert;
-  sendWarn: AlertTypes.ISendAlert;
-  sendSuccess: AlertTypes.ISendAlert;
+  public alert: Alert;
 
   constructor() {
-    this.agent = this.initAgentClient();
-    // DEV NOTE -> Do we need this?
-    this.sendErrorChannel = new Alert().sendErrorChannel;
-    this.sendError = new Alert().sendError;
-    this.sendInfo = new Alert().sendInfo;
-    this.sendWarn = new Alert().sendWarn;
-    this.sendSuccess = new Alert().sendSuccess;
-  }
-
-  private initAgentClient() {
-    return axios.create({
-      headers: { "Content-Type": "application/json" },
-    });
+    this.agent = axios.create({ headers: { "Content-Type": "application/json" } });
+    this.alert = new Alert();
   }
 
   async getNode(id: string): Promise<INode> {
@@ -39,101 +22,99 @@ export default class Service {
       .exec();
   }
 
-  async getLoadBalancers(loadBalancers: string[]): Promise<LoadBalancer[]> {
-    return await HostsModel.find({ _id: { $in: loadBalancers } }).exec();
+  /* ----- Toggle Server Methods ----- */
+  async enableServer({ backend, server, loadBalancers }: IRotationParams) {
+    try {
+      const status = await this.getServerStatus({ backend, server, loadBalancers });
+      if (status === LoadBalancerStatus.ONLINE) {
+        const message = this.getErrorMessage(server, "online");
+        await this.alert.sendErrorChannel({ title: backend, message });
+        throw message;
+      }
+
+      return (
+        await Promise.all(
+          loadBalancers.map(({ ip }) =>
+            this.agent.post<{ status: string }>(`http://${ip}:3001/webhook/lb/enable`, {
+              backend,
+              server,
+            }),
+          ),
+        )
+      ).every(({ data }) => Boolean(data?.status));
+    } catch (error) {
+      const message = `Could not contact agent to enable server. ${error}`;
+      await this.alert.sendErrorChannel({ title: backend, message });
+    }
   }
 
-  async disableServer({
+  async disableServer({ backend, server, loadBalancers }: IRotationParams) {
+    try {
+      const count = await this.getServerCount({ backend, loadBalancers });
+      if (count <= 1) {
+        const message = this.getErrorMessage(server, "count", count);
+        await this.alert.sendErrorChannel({ title: backend, message });
+        throw message;
+      }
+
+      const status = await this.getServerStatus({ backend, server, loadBalancers });
+      if (status === LoadBalancerStatus.OFFLINE) {
+        const message = this.getErrorMessage(server, "offline");
+        await this.alert.sendErrorChannel({ title: backend, message });
+        throw message;
+      }
+
+      return (
+        await Promise.all(
+          loadBalancers.map(({ ip }) =>
+            this.agent.post<{ status: string }>(`http://${ip}:3001/webhook/lb/disable`, {
+              backend,
+              server,
+            }),
+          ),
+        )
+      ).every(({ data }) => Boolean(data?.status));
+    } catch (error) {
+      await this.alert.sendErrorChannel({ title: backend, message: error });
+      throw new Error(error);
+    }
+  }
+
+  /* ----- Server Check Methods ----- */
+  private async getServerStatus({
     backend,
     server,
     loadBalancers,
-  }: IRotationParams): Promise<void> {
-    try {
-      const count = await this.getBackendServerCount({ backend, loadBalancers });
-      if (count <= 1) {
-        await this.sendErrorChannel({
-          title: backend,
-          message: `Could not remove ${server} from load balancer, ${count} server online.\nManual intervention required.`,
-        });
-        return;
-      }
-
-      const status = await this.getBackendServerStatus({
-        backend,
-        server,
-        loadBalancers,
-      });
-      if (status === LoadBalancerStatus.OFFLINE) {
-        await this.sendErrorChannel({
-          title: backend,
-          message: `Could not remove from load balancer, server already offline.`,
-        });
-        return;
-      }
-
-      await Promise.all(
-        loadBalancers.map(async ({ ip }) =>
-          this.agent.post(`http://${ip}:3001/webhook/lb/disable`, {
-            backend,
-            server,
-          }),
-        ),
-      );
-    } catch (error) {
-      await this.sendErrorChannel({
-        title: backend,
-        message: `Could not remove from load balancer. ${error}`,
-      });
-    }
-  }
-
-  async enableServer({ backend, server, loadBalancers }: IRotationParams): Promise<void> {
-    try {
-      await Promise.all(
-        loadBalancers.map(({ ip }) =>
-          this.agent.post(`http://${ip}:3001/webhook/lb/enable`, { backend, server }),
-        ),
-      );
-    } catch (error) {
-      await this.sendErrorChannel({
-        title: backend,
-        message: `Could not contact agent to enable server. ${error}`,
-      });
-    }
-  }
-
-  async getBackendServerStatus({ backend, server, loadBalancers }: IRotationParams) {
-    const results = [];
-
+  }: IRotationParams): Promise<LoadBalancerStatus> {
+    const results: boolean[] = [];
     for (const { ip } of loadBalancers) {
       try {
-        const { data } = await this.agent.post(`http://${ip}:3001/webhook/lb/status`, {
-          backend,
-          server,
-        });
-        results.push(data);
+        const { data } = await this.agent.post<{ status: boolean }>(
+          `http://${ip}:3001/webhook/lb/status`,
+          { backend, server },
+        );
+        results.push(data.status);
       } catch (error) {
-        throw new Error(`Could not get backend status, ${ip} ${backend} ${error}`);
+        throw `Could not get backend status.\nIP: ${ip} Backend: ${backend} ${error}`;
       }
     }
 
-    if (results.every(({ status }) => status === true)) {
+    console.debug("INSIDE BACKEND STATUS CHECK", { results });
+
+    if (results.every((status) => status === true)) {
       return LoadBalancerStatus.ONLINE;
     }
-
-    if (results.every(({ status }) => status === false)) {
+    if (results.every((status) => status === false)) {
       return LoadBalancerStatus.OFFLINE;
     }
-
     return LoadBalancerStatus.ERROR;
   }
 
-  async getBackendServerCount({
+  private async getServerCount({
     backend,
     loadBalancers,
   }: IRotationParams): Promise<number> {
     const results: number[] = [];
-
     for await (const { ip } of loadBalancers) {
       try {
         const { data } = await this.agent.post<{ status: number }>(
@@ -142,7 +123,7 @@ export default class Service {
         );
         results.push(data.status);
       } catch (error) {
-        throw new Error(`Could not get backend status: ${ip} ${backend} ${error}`);
+        throw `Could not get backend status.\nIP: ${ip} Backend: ${backend} ${error}`;
       }
     }
 
@@ -152,10 +133,25 @@ export default class Service {
     return -1;
   }
 
+  /* ----- Message String Methods ----- */
   getHAProxyMessage({ backend, loadBalancers }: IRotationParams): string {
     const urls = loadBalancers
       .map(({ ip }) => `http://${ip}:8050/stats/;up?scope=${backend}\n`)
       .join("");
     return `HAProxy Status\n${urls}`;
+  }
+
+  private getErrorMessage(
+    server: string,
+    mode: "count" | "offline" | "online",
+    count?: number,
+  ): string {
+    return {
+      count: `Could not remove ${server} from load balancer. ${
+        count === 1 ? "Only one server" : "No servers"
+      } online.\nManual intervention required.`,
+      offline: `Could not remove ${server} from load balancer. Server already offline.`,
+      online: `Could not add ${server} to load balancer. Server already online.`,
+    }[mode];
   }
 }
