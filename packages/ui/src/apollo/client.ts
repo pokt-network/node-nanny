7b40a3fb78fbe7d6120c83606a5502cec01424f2
Pascal van Leeuwen <pascal@pokt.network>
2022-03-18 20:59:05 +0000

    Implemented infinite scroll for Logs table.

---
+++ packages/ui/src/apollo/client.ts
@@ -0,0 +1,34 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { IPaginatedLogs } from "../types";
+
+export default new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache({
+    addTypename: true,
+    typePolicies: {
+      Query: {
+        fields: {
+          logs: {
+            keyArgs: false,
+            /* Merge new Logs into cache when fetching paginated logs for infinite scrolling LogTable */
+            merge(
+              existing: IPaginatedLogs = {
+                docs: [],
+                hasNextPage: false,
+                hasPrevPage: false,
+                limit: 0,
+                page: 0,
+                totalDocs: 0,
+                totalPages: 0,
+                pagingCounter: 0,
+              },
+              incoming: IPaginatedLogs,
+            ) {
+              return { ...incoming, docs: [...existing.docs, ...incoming.docs] };
+            },
+          },
+        },
+      },
+    },
+  }),
+});
