4aea908ac9dc180c7bb4ae0ea76f14de495742d4
Pascal van Leeuwen <pascal@pokt.network>
2022-03-18 20:59:05 +0000

    Implemented infinite scroll for Logs table.

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,33 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache({
-    addTypename: true,
-    typePolicies: {
-      Query: {
-        fields: {
-          logs: {
-            keyArgs: false,
-            merge(existing = { docs: [] }, incoming) {
-              console.log({ existing, incoming });
-              return { ...incoming, docs: [...existing.docs, ...incoming.docs] };
-            },
-          },
-        },
-      },
-    },
-  }),
-});
+import apolloClient from "./apollo/client";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
@@ -35,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
