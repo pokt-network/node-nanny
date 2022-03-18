e05a9a93f9d2ff16e1f397a689e21a91c4947918
Pascal van Leeuwen <pascal@pokt.network>
2022-03-18 00:06:24 +0000

    Created Logs page in UI and query to fetch by multiple nodes.

--- packages/core/src/services/automation/automation.test.ts
+++ packages/core/src/services/automation/automation.test.ts
@@ -15,7 +15,7 @@ describe("Automation Service Tests", () => {
   describe("Get Logs Tests", () => {
     test("Should fetch logs for a specific Node without timestamp query", async () => {
       const logsForNode = await automationService.getLogsForNode({
-        nodeId: "622faff77d73779113cb8012",
+        nodeIds: ["622faff77d73779113cb8012"],
       });
 
       expect(logsForNode).toBeTruthy();
@@ -24,7 +24,7 @@ describe("Automation Service Tests", () => {
 
     test("Should fetch logs for a specific Node with timestamps", async () => {
       const logsForNode = await automationService.getLogsForNode({
-        nodeId: "622faff77d73779113cb8012",
+        nodeIds: ["622faff77d73779113cb8012"],
         startDate: "2022-03-16T21:43:36.367+00:00",
         endDate: "2022-03-18T21:43:36.367+00:00",
       });
