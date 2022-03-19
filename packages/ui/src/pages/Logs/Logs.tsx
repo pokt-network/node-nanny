f0256d9a6fc307a4c1cc2faddf0729751507be7c
Pascal van Leeuwen <pascal@pokt.network>
2022-03-19 00:47:43 +0000

    Updated log display. Set up chart.js to display logs.

--- packages/ui/src/pages/Logs/Logs.tsx
+++ packages/ui/src/pages/Logs/Logs.tsx
@@ -13,13 +13,89 @@ import {
 import { LogTable } from "components";
 import { ILogsQuery, IParsedLog, useLogsQuery, useNodesQuery } from "types";
 
+import LogsChart from "./LogsChart";
+
 const ONE_MINUTE = 60 * 1000;
 const ONE_HOUR = ONE_MINUTE * 60;
 const ONE_DAY = ONE_HOUR * 24;
 
+export interface ITimePeriod {
+  code: string;
+  label: string;
+  timePeriod: number;
+  resolution: number;
+  format: string;
+}
+
+const timePeriods: ITimePeriod[] = [
+  {
+    code: "15M",
+    label: "Past 15 Minutes",
+    timePeriod: ONE_MINUTE * 15,
+    resolution: ONE_MINUTE,
+    format: "HH:mm",
+  },
+  {
+    code: "1H",
+    label: "Past 2 Hours",
+    timePeriod: ONE_HOUR,
+    resolution: ONE_MINUTE * 5,
+    format: "HH:mm",
+  },
+  {
+    code: "4H",
+    label: "Past 4 Hours",
+    timePeriod: ONE_HOUR * 4,
+    resolution: ONE_MINUTE * 15,
+    format: "HH:mm",
+  },
+  {
+    code: "1D",
+    label: "Past 1 Day",
+    timePeriod: ONE_DAY,
+    resolution: ONE_HOUR,
+    format: "HH:mm",
+  },
+  {
+    code: "2D",
+    label: "Past 2 Days",
+    timePeriod: ONE_DAY * 2,
+    resolution: ONE_HOUR * 3,
+    format: "HH:mm",
+  },
+  {
+    code: "3D",
+    label: "Past 3 Days",
+    timePeriod: ONE_DAY * 3,
+    resolution: ONE_HOUR * 6,
+    format: "HH:mm",
+  },
+  {
+    code: "7D",
+    label: "Past 7 Days",
+    timePeriod: ONE_DAY * 7,
+    resolution: ONE_HOUR * 12,
+    format: "HH:mm",
+  },
+  {
+    code: "15D",
+    label: "Past 15 Days",
+    timePeriod: ONE_DAY * 15,
+    resolution: ONE_DAY,
+    format: "ddd MMM DD",
+  },
+  {
+    code: "1MO",
+    label: "Past 1 Month",
+    timePeriod: ONE_DAY * 30,
+    resolution: ONE_DAY * 2,
+    format: "ddd, MMM DD",
+  },
+];
+
 export function Logs() {
   const [nodes, setNodes] = useState<string[]>([]);
-  const [startDate, setStartDate] = useState<number>(ONE_MINUTE * 15);
+  const [logPeriod, setLogPeriod] = useState<ITimePeriod>(timePeriods[0]);
   const [logsLoading, setLogsLoading] = useState(false);
 
   const { data: nodesData, error: nodesError, loading: nodesLoading } = useNodesQuery();
@@ -38,6 +114,11 @@ export function Logs() {
     setLogsLoading(true);
     refetch();
   }, [nodes]);
+
+  const handleTimePeriodChange = ({ target }: SelectChangeEvent<string>) => {
+    const { value } = target;
+    setLogPeriod(timePeriods.find(({ code }) => code === value)!);
+  };
 
   const handleNodesChange = ({ target }: SelectChangeEvent<typeof nodes>) => {
     const { value } = target;
@@ -82,22 +163,24 @@ export function Logs() {
           ))}
         </Select>
       </FormControl>
-      {/* <div style={{ marginTop: "10px" }} />
+      <div style={{ marginTop: "10px" }} />
       <FormControl fullWidth>
-        <InputLabel id="chain-label">Chain</InputLabel>
+        <InputLabel id="chain-label">Time Period</InputLabel>
         <Select
           labelId="chain-label"
-          value={chain}
-          label="Chain"
-          onChange={handleChainChange}
+          value={logPeriod.code}
+          label="Time Period"
+          onChange={handleTimePeriodChange}
         >
-          {formData?.chains.map(({ name, id }) => (
-            <MenuItem key={id} value={id}>
-              {name}
+          {timePeriods.map(({ code, label }) => (
+            <MenuItem key={code} value={code}>
+              {label}
             </MenuItem>
           ))}
         </Select>
-      </FormControl> */}
+      </FormControl>
+      <div style={{ marginTop: "10px" }} />
+      {logsData && <LogsChart logs={logsData.logs.docs} logPeriod={logPeriod} />}
       <div style={{ marginTop: "10px" }} />
       {logsData && (
         <LogTable
