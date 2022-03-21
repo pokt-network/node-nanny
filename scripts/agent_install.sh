f6d6dcb3ca1d5b1dcf4e6a87a73c76034b350325
Pascal van Leeuwen <47662958+commoddity@users.noreply.github.com>
2022-03-21 12:32:30 -0700

    Update agent_install.sh

--- scripts/agent_install.sh
+++ scripts/agent_install.sh
@@ -2,7 +2,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 16
-npm install yarn pm2 -g
+npm install pm2 -g
 git clone https://github.com/pokt-foundation/node-nanny.git
 cd node-nanny
 chmod +x scripts/agent_reboot.sh
