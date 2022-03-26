2e9e0f9348d408a9db52aee1223d21401db46faa
Pascal van Leeuwen <pascal@pokt.network>
2022-03-26 02:06:11 +0000

    Clean up unused folders/files from codebase and updated dockerfile.

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-cd /home/ubuntu/run
-pm2 kill
-yarn
-yarn build
-pm2 start dist --cron-restart="0 * * * *"
-pm2 start dist/api
