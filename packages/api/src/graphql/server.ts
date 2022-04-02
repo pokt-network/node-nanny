6832c0a1de05f69756dd29ce3b9dfc9ca5f8dd26
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-01 18:24:22 -0700

    Setup CORS settings in server.

--- packages/api/src/graphql/server.ts
+++ packages/api/src/graphql/server.ts
@@ -10,7 +10,18 @@ import typeDefs from "./schema";
   await connect();
   console.log(`MongoDB connection established ...`);
 
-  const { url } = await new ApolloServer({ typeDefs, resolvers }).listen({ port: 4000 });
+  const hostname = process.env.HOSTNAME || "localhost";
+  const port = process.env.API_PORT || 4000;
+  const uiPort = process.env.UI_PORT || 3000;
+  const cors = {
+    origin: `http://${hostname}:${uiPort}`,
+    credentials: true,
+  };
+
+  const { url } = await new ApolloServer({ typeDefs, resolvers, cors }).listen({
+    hostname,
+    port,
+  });
   console.log(`🚀 GraphQL server ready at ${url}`);
 })();
 
