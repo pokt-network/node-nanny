6cb39013ea0508ef47d9f8d49a3245cf971f0ec1
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-04 14:26:00 -0700

    Improved updater script and added creation of frontend webhook to it.

--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -1,5 +1,6 @@
 export { Service as Alert } from "./alert";
 export { Service as Automation } from "./automation";
+export { Service as DiscordService } from "./discord";
 export { Service as Event } from "./event";
 export { Service as HAProxy } from "./haproxy";
 export { Service as Health } from "./health";
