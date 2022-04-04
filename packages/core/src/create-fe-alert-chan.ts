1a5ecf6eb46de04a7007aca5eb33ef33a24adb38
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-04 14:19:56 -0700

    Updated init db script.

--- packages/core/src/create-fe-alert-chan.ts
+++
@@ -1,9 +0,0 @@
-import { Service as DiscordService } from "./services/discord";
-
-(async () => {
-  try {
-    await new DiscordService().addWebhookForFrontendNode();
-  } catch {
-    // Do nothing
-  }
-})();
