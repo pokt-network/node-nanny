aa3d4cd9d32149e11de930e4cc32a6d01c855cad
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-04 13:33:15 -0700

    Removed datadog, infra and event services as they are now moved to infra support lambda.

--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -1,8 +1,6 @@
 export { Service as Alert } from "./alert";
 export { Service as Automation } from "./automation";
-export { Service as DataDog } from "./datadog";
 export { Service as Event } from "./event";
 export { Service as HAProxy } from "./haproxy";
 export { Service as Health } from "./health";
 export { Service as Log } from "./log";
-export { Service as Infra } from "./infra";
