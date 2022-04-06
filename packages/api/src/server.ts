f4d5ce5a440749df7bcbf34fdf6df0efb3a05100
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-05 17:53:13 -0700

    Setup UI container separate from backend.

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -10,21 +10,10 @@ import typeDefs from "./schema";
   await connect();
   console.log(`MongoDB connection established ...`);
 
-  const backendHost = process.env.BACKEND_HOST || "localhost";
-  const frontendHost = process.env.FRONTEND_HOST || "localhost";
-  const port = process.env.API_PORT || 4000;
-  const uiPort = process.env.PORT || 3000;
-  const cors = {
-    origin: "*",
-    // origin: `http://${frontendHost}:${uiPort}`,
-    credentials: true,
-  };
-
-  await new ApolloServer({ typeDefs, resolvers, cors }).listen({
-    frontendHost,
-    port,
-  });
-  console.log(`🚀 GraphQL server ready at ${`http://${backendHost}:${port}`}`);
+  const cors = { origin: true, credentials: true };
+  const port = 4000;
+  await new ApolloServer({ typeDefs, resolvers, cors }).listen({ port });
+  console.log(`🚀 GraphQL server listening on ${port}`);
 })();
 
 process.on("SIGINT", function () {
