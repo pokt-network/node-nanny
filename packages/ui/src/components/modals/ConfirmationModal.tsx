7bf564954b2b86c9155d1d3d328db5ea4bf191b2
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-04-12 16:25:10 -0700

    Updated delete confirmation modal text and colors.

--- packages/ui/src/components/modals/ConfirmationModal.tsx
+++ packages/ui/src/components/modals/ConfirmationModal.tsx
@@ -5,19 +5,53 @@ import { ModalHelper } from "utils";
 export interface ConfirmationModalProps {
   handleOk: any;
   promptText: string;
+  okText?: string;
+  okColor?:
+    | "inherit"
+    | "success"
+    | "error"
+    | "primary"
+    | "secondary"
+    | "info"
+    | "warning";
+  cancelColor?:
+    | "inherit"
+    | "success"
+    | "error"
+    | "primary"
+    | "secondary"
+    | "info"
+    | "warning";
 }
 
-export function ConfirmationModal({ handleOk, promptText }: ConfirmationModalProps) {
+export function ConfirmationModal({
+  handleOk,
+  promptText,
+  okText,
+  okColor,
+  cancelColor,
+}: ConfirmationModalProps) {
   return (
     <>
-      <Paper style={{ width: "100%", padding: 10 }} variant="outlined">
+      <Paper style={{ width: "100%", padding: 24 }} variant="outlined" color="success">
         <Typography>{promptText}</Typography>
-        <div>
-          <Button onClick={handleOk} variant="contained">
-            Ok
-          </Button>
-          <Button onClick={() => ModalHelper.close()} variant="contained">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 16,
+            padding: "0px 16px",
+          }}
+        >
+          <Button
+            onClick={() => ModalHelper.close()}
+            variant="contained"
+            color={cancelColor || "error"}
+          >
             Cancel
+          </Button>
+          <Button onClick={handleOk} variant="contained" color={okColor || "success"}>
+            {okText || "OK"}
           </Button>
         </div>
       </Paper>
