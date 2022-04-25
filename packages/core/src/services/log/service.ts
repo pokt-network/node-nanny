63e19cf349fea15e69e8f699ac3e5433cd6f0b9a
Pascal van Leeuwen <pascal@pokt.network>
2022-04-25 21:44:21 +0000

    Updated env naming.

--- packages/core/src/services/log/service.ts
+++ packages/core/src/services/log/service.ts
@@ -5,13 +5,13 @@ import { FilterQuery } from "mongoose";
 import { LogsModel, ILog, IPaginatedLogs } from "../../models";
 import { INodeLogParams, ILogChartParams, ILogForChart } from "./types";
 
-import Env from "../../environment";
+import env from "../../environment";
 
 export class Service {
   public init(id: string, name: string): Logger {
     const transport = {
       mongodb: new transports.MongoDB({
-        db: Env("MONGO_URI"),
+        db: env("MONGO_URI"),
         expireAfterSeconds: 60,
         label: id,
         collection: "logs",
@@ -19,12 +19,12 @@ export class Service {
       }),
       datadog: new transports.Http({
         host: "http-intake.logs.datadoghq.eu",
-        path: `/api/v2/logs?dd-api-key=${Env(
+        path: `/api/v2/logs?dd-api-key=${env(
           "DD_API_KEY",
         )}&ddsource=nodejs&service=node-nanny/${name}`,
         ssl: true,
       }),
-    }[Env("MONITOR_LOGGER")];
+    }[env("MONITOR_LOGGER")];
 
     return createLogger({
       level: "info",
