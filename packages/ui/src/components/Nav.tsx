c337293f56f5efa9581e0b45c7f4762c302ddb9b
Kevin Utoft <kevin.utoft@gmail.com>
2022-04-26 12:45:33 -0500

    T-2557: update nav order

--- packages/ui/src/components/Nav.tsx
+++ packages/ui/src/components/Nav.tsx
@@ -11,47 +11,48 @@ import { useLocation } from "react-router-dom";
 const data = [
   {
     label: "Logs",
-    to: '/logs',
+    to: "/logs",
     icon: BarChartIcon,
   },
   {
-    label: "Hosts",
-    to: '/hosts',
-    icon: PeopleIcon,
+    label: "Nodes",
+    to: "/nodes",
+    icon: LayersIcon,
   },
   {
-    label: "Nodes",
-    to: '/nodes',
-    icon: LayersIcon,
-  }
-]
+    label: "Hosts",
+    to: "/hosts",
+    icon: PeopleIcon,
+  },
+];
 
 export const Nav = () => {
   const location = useLocation();
   return (
     <List>
-      {data && data.map(navItem => (
-        <ListItemButton 
-          key={navItem.label} 
-          component={Link} 
-          href={navItem.to} 
-          color="inherit" 
-          selected={navItem.to === location.pathname}
-          sx={{
-            '&.Mui-selected': {
-              backgroundColor: '#2E3643',
-            },
-            '&.Mui-selected:hover': {
-              backgroundColor: 'rgba(255, 255, 255, 0.08)',
-            }
-          }}
-        >
-          <ListItemIcon>
-            <navItem.icon />
-          </ListItemIcon>
-          <ListItemText primary={navItem.label} />
-        </ListItemButton>
-      ))}
+      {data &&
+        data.map((navItem) => (
+          <ListItemButton
+            key={navItem.label}
+            component={Link}
+            href={navItem.to}
+            color="inherit"
+            selected={navItem.to === location.pathname}
+            sx={{
+              "&.Mui-selected": {
+                backgroundColor: "#2E3643",
+              },
+              "&.Mui-selected:hover": {
+                backgroundColor: "rgba(255, 255, 255, 0.08)",
+              },
+            }}
+          >
+            <ListItemIcon>
+              <navItem.icon />
+            </ListItemIcon>
+            <ListItemText primary={navItem.label} />
+          </ListItemButton>
+        ))}
     </List>
   );
 };
