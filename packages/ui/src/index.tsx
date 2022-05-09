35f2129e1a12efeecf9d8bdc23da0600f3b28c98
Pascal van Leeuwen <47662958+commoddity@users.noreply.github.com>
2022-05-09 13:30:32 -0700

    Revert "T-2349 - Pascal/time to recover"

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,17 +1,18 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 
 import apolloClient from "./apollo/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <ApolloProvider client={apolloClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
