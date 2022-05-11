22e854b6108af16fd12b54054d6e65113f5d0fc0
Pascal van Leeuwen <pascal@pokt.network>
2022-05-11 22:47:16 +0000

    Added chainID to chains model, schema, types and queries.

---
+++ packages/ui/src/components/PNF/PNFStatus.tsx
@@ -0,0 +1,81 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+import Paper from 'components/Paper';
+import Title from 'components/Title';
+import { HostActionsState } from 'pages/Hosts';
+import HostForm from './ChainsForm';
+import { IHost, IHostsQuery, ILocation } from 'types';
+
+import Box from '@mui/material/Box';
+import { ApolloQueryResult } from '@apollo/client';
+
+interface PNFStatusProps {
+  locations: ILocation[];
+  hostNames: string[];
+  hostsWithNode: { [id: string]: number };
+  host: IHost;
+  type: HostActionsState;
+  setState: Dispatch<HostActionsState>;
+  setSelectedHost: Dispatch<SetStateAction<IHost>>;
+  refetch: (variables?: any) => Promise<ApolloQueryResult<IHostsQuery>>;
+}
+
+export const PNFStatus = ({
+  host,
+  locations,
+  hostNames,
+  hostsWithNode,
+  type,
+  setState,
+  setSelectedHost,
+  refetch,
+}: PNFStatusProps) => {
+  const [title, setTitle] = useState('Select Host To View Status');
+
+  useEffect(() => {
+    if (type === 'create') {
+      setTitle('Create Host');
+    }
+    if (type === 'edit') {
+      setTitle('Edit Host');
+    }
+    if (type === 'info') {
+      setTitle('Select Host To View Status');
+    }
+    if (type === 'info' && host) {
+      setTitle('Selected Host');
+    }
+  }, [host, type]);
+
+  return (
+    <Paper>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          width: '100%',
+          '& h3': { textTransform: 'capitalize' },
+        }}
+      >
+        <Title>{title}</Title>
+      </Box>
+      <Box>
+        <HostForm
+          read={type === 'info'}
+          update={type === 'info' || type === 'edit'}
+          locations={locations}
+          hostNames={hostNames}
+          hostsWithNode={hostsWithNode}
+          refetchHosts={refetch}
+          setSelectedHost={setSelectedHost}
+          selectedHost={type !== 'create' ? host : null}
+          onCancel={() => setState(HostActionsState.Info)}
+          setState={setState}
+        />
+      </Box>
+    </Paper>
+  );
+};
+
+export default PNFStatus;
--- packages/ui/src/pages/index.ts
+++ packages/ui/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Logs } from './Logs';
 export { Hosts } from './Hosts';
 export { Nodes } from './Nodes';
+export { PNF } from './PNF';
