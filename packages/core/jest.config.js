9ec055df557e8b00c8dd4232d658c68a14650efc
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-06-03 20:23:11 -0700

    feat(monitor): initial commit to branch of rework of Health service to be Chain-agnostic, with comprehensive mocked unit test coverage.

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,7 +1,7 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
-  preset: '@shelf/jest-mongodb',
+  // preset: '@shelf/jest-mongodb',
   verbose: true,
   testTimeout: 100000000,
   transform: tsjPreset.transform,
