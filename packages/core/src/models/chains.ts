8ce0f1e131d390a6c0195eaad5943e12b104057d
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-06-07 16:12:10 -0700

    Stash.

--- packages/core/src/models/chains.ts
+++ packages/core/src/models/chains.ts
@@ -4,18 +4,31 @@ export interface IChain {
   id: Types.ObjectId;
   name: string;
   type: string;
+  /** The relay chain ID - used for visual reference in the UI only. Not used in backend code. */
   chainId: string;
+  /** The amount of blocks the node can be behind the reference (oracle or peer) before being considered out of sync */
   allowance: number;
 
-  /** The HTTP method the health check for the Chain uses */
+  /** The HTTP method that the health check for the chain uses. "get" or "post" */
   method: string;
+  /** The RPC request body used for the chain health check. MUST BE VALID JSON.
+   * - ex: '{ "jsonrpc": "2.0", "id": 1, "method": "eth_blockNumber", "params": [] }' */
   rpc: string;
+  /** The object path that contains the health check response field. RESPONSE IS AN AxiosResponse SO TOP LEVEL IS "status", "data", etc.
+   * - ex: "status" or "data.result.healthy" */
   responsePath: string;
-  healthyValue: string;
+  /** Whether the chain provides its own endpoint for health checks. */
   hasOwnEndpoint: boolean;
+  /** hasOwnEndpoint = true ONLY. The value used to determine if a node is healthy.
+   * - ex: "healthy" or "200" or "true" */
+  healthyValue: string;
+  /** Whether the chain uses external RPC endpoints (oracles) to check block height. */
   useOracles: boolean;
+  /** (Optional) The URL path appended to the node's URL to check health. MUST INCLUDE LEADING SLASH.
+   * - ex: "/health" or "/v1/query/height" or "/status" */
   endpoint?: string;
 
+  /** Used by the `updater` job to determine if there are newer Chains in the Node Nanny prod DB */
   updatedAt: string;
 }
 
