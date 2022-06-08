496edf6bec25f405dd1ad7c45f83aad242e9d6e4
Pascal van Leeuwen <pascalvanleeuwen604@gmail.com>
2022-06-07 17:05:33 -0700

    feat(dev): pushing to dev docker image.

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,7 +1,10 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
-  // preset: '@shelf/jest-mongodb',
+  // NOTE -> Currently the jest-mongodb package has a bug where it doesn't work on Ubuntu 22.04
+  // so if needing to run tests locally on Ubuntu 22.04, the following line must be commented out
+  // and test files that use the in-memory DB are not supported. Tests work fine in the GH Actions CI
+  preset: '@shelf/jest-mongodb',
   verbose: true,
   testTimeout: 100000000,
   transform: tsjPreset.transform,
